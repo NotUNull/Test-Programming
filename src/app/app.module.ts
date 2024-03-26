be07c9369da92d96a6e50a4aef9bedb10c4d7395
NzlNegative2 <anut.remember@gmail.com>
2024-03-27 06:27:42 +0700

    add code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InitialComponent } from './initial/initial.component';
+import { ResultComponent } from './result/result.component';
+import { FormComponent } from './result/form/form.component';
+import { TableComponent } from './result/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialComponent,
+    ResultComponent,
+    FormComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
